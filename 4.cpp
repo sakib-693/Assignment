4b89237517f0e770119c2f7d9b0204a8e5f8c61d
sakib-693 <133006327+sakib-693@users.noreply.github.com>
2023-05-09 21:29:15 +0530

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float c,f;
+printf("enter the temoerature in celcius");
+scanf("%f",&c);
+f=((9/5*c)+32);
+printf("temperature in farenhite is : %f",f);
+return 0;
+}
