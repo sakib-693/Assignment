aa01c8d32a5fd186f0b127bcdaf33312b489dafa
sakib-693 <133006327+sakib-693@users.noreply.github.com>
2023-05-09 21:09:36 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+int p,r,t,SI;
+printf("enter the princple");
+scanf("%d",&p);
+printf("enter the rate");
+scanf("%d",&r);
+printf("enter time");
+scanf("%d",&t);
+SI=(p*r*t);
+printf("Simple Intrest= %d",SI);
+
+return 0;
+}
+
