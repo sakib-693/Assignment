422f8a257bff9c0e699a5857eb3be7ff5f113dc2
sakib-693 <133006327+sakib-693@users.noreply.github.com>
2023-05-09 21:56:16 +0530

    Create 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter first number :");
+scanf("%d",&a);
+printf("enter second number :");
+scanf("%d",&b);
+if(a==b){
+printf("both numbers are equal");
+}
+else{
+    printf("both numbers are not equal");
+}
+return 0;
+}
